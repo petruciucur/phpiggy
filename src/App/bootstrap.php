2be6378f85444745110c0d9cf7070aa676e076ec
petruciucur <petruciucur@yahoo.com>
2023-09-17 14:39:12 +0300

    Add files via upload

---
+++ src/App/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+// include __DIR__ . "/../Framework/App.php";
+// After composer autoloading
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Framework\App;
+$app = new App();
+return $app;
